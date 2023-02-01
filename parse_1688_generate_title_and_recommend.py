b00bc175020ebbb82654e0e6b87c4edfc05d390b
熊鹰扬 <jakrinchose@gmail.com>
2023-02-01 18:02:47 +0800

    update

--- parse_1688_generate_title_and_recommend.py
+++ parse_1688_generate_title_and_recommend.py
@@ -4,17 +4,33 @@ import time
 import datetime
 import json
 
-openai.api_key = "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"
+openai.api_key = "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"
+
+prompt_generate_product_title = "this is some information of a product in json format, \
+    please help to genenrate a product title in english: %s %s"
+
+prompt_extract_item_specifies = "this is some information of a product in json format, \
+    please list out all the specifies in english: %s %s"
+
+prompt_expand_tags = "this is some information of a product in json format\
+    , please extract no more than 20 most important tags for online selling in English\
+    , and for every tag please help me to list out most usual synonyms \
+    : %s %s"
+    
+prompt_generate_recommend_essay = "this is some information of a product in json format, \
+    please help to generate a short recomend text in english, for online selling: %s %s"
+    
+prompt_generate_keywords_of_selling_points = "here are a list of products from a online store, \
+    please extract out 10 keywords as the best selling points for this store: %s"
 
 def parse_product(item_id, title, item_specifies):
     print(item_id)
     
-    prompt = "this is some information of a product in json format, please help to genenrate a product title in english: %s %s"%(title, json.dumps(item_specifies))
     #print ("...%s..."%prompt)
     response = openai.Completion.create(
       model="text-davinci-003",
-      prompt=prompt,
-      max_tokens=300,
+      prompt=prompt_generate_product_title%(title, json.dumps(item_specifies)),
+      max_tokens=128,
       top_p=1.0,
       frequency_penalty=0.0,
       presence_penalty=0.0
@@ -23,40 +39,36 @@ def parse_product(item_id, title, item_specifies):
     print ("the original title : %s"%title)
     print ("the title generate by GPT-3 is: %s"%gpt_title.strip())
 
-#    print("-----------------------------------------------------------")
-#    prompt = "this is some information of a product in json format, please list out all the specifies in english: %s %s"%(title, json.dumps(item_specifies))
-#    response = openai.Completion.create(
-#      model="text-davinci-003",
-#      prompt=prompt,
-#      max_tokens=1000,
-#      top_p=1.0,
-#      frequency_penalty=0.0,
-#      presence_penalty=0.0
-#    )
-#    print(response.choices[0]['text'])
-#
-#    prompt = "this is some information of a product in json format, please help to generate a short recomend text in english, for online selling: %s %s"%(title, json.dumps(item_specifies))
-#    response = openai.Completion.create(
-#      model="text-davinci-003",
-#      prompt=prompt,
-#      max_tokens=300,
-#      top_p=1.0,
-#      frequency_penalty=0.0,
-#      presence_penalty=0.0
-#    )
-#    print(response.choices[0]['text'])
-#
-#    print("-----------------------------------------------------------")
-#    prompt = "this is some information of a product in json format, please help to generate three paragraphs of recomend text in english, for online selling: %s %s"%(title, json.dumps(item_specifies))
-#    response = openai.Completion.create(
-#      model="text-davinci-003",
-#      prompt=prompt,
-#      max_tokens=300,
-#      top_p=1.0,
-#      frequency_penalty=0.0,
-#      presence_penalty=0.0
-#    )
-#    print(response.choices[0]['text'])
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=prompt_extract_item_specifies%(title, json.dumps(item_specifies)),
+      max_tokens=1024,
+      top_p=1.0,
+      frequency_penalty=0.0,
+      presence_penalty=0.0
+    )
+    print(response.choices[0]['text'])
+    
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=prompt_expand_tags%(title, json.dumps(item_specifies)),
+      max_tokens=1024,
+      top_p=1.0,
+      frequency_penalty=0.0,
+      presence_penalty=0.0
+    )
+    print(response.choices[0]['text'])
+
+
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=prompt_generate_recommend_essay%(title, json.dumps(item_specifies)),
+      max_tokens=256,
+      top_p=1.0,
+      frequency_penalty=0.0,
+      presence_penalty=0.0
+    )
+    print(response.choices[0]['text'])
 
     return gpt_title
 
@@ -70,12 +82,10 @@ with open("1688_product_test.json", "r") as file_in:
         gpt_title = parse_product(product_json['itemId'], product_json['itemName'], product_json['itemSpecifics'])
         gpt_title_list.append(gpt_title)
     
-    title_string = ','.join(gpt_title_list)
-    prompt = "here are a list of products from a online store, please extract out 10 keywords as the best selling points for this store: %s"%title_string
     response = openai.Completion.create(
       model="text-davinci-003",
-      prompt=prompt,
-      max_tokens=300,
+      prompt=prompt_generate_keywords_of_selling_points%','.join(gpt_title_list),
+      max_tokens=512,
       top_p=1.0,
       frequency_penalty=0.0,
       presence_penalty=0.0
