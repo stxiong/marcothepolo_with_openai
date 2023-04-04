1c0a34e365685ed32976a69cbbb64e7f572e3b89
熊鹰扬 <jakrinchose@gmail.com>
2023-04-04 17:56:33 +0800

    update saturn engine

--- saturn_engine/product_generator.py
+++ saturn_engine/product_generator.py
@@ -26,129 +26,39 @@ class ProductContextGenerator(object):
         self.item_specifics = ProductContextGenerator.parse_specific_dict(self.product_info_dict['item_specifics'])
         self.category = None
         
-    def __generate_title(self, max_tokens, temperature, language='English'):
+    def __generate_basic_product_context(self, language='English'):
         
-        prompt = "this is some information of a product in json format,"
-        if self.category != None:
-            prompt += " and this is a %s product"%self.category 
-        prompt += " please help to genenrate a product title in english," 
-        prompt += " the title should only include %s words,"%language
-        prompt += " and please notice that don't include any information about 'cross-border'"
-        prompt += " : %s %s."%(self.item_name, json.dumps(self.item_specifics))
+        prompt = 'this is some information of a product in strict json format: [%s],'%json.dumps(self.item_specifics)
+        prompt += 'and the item name in Chinese is: [%s]. \n'%self.item_name
+        prompt += ' help to generate one product title for this item no more than 35 words,'
+        prompt += ' and generate one short title for this item with no more than 45 charactors, '
+        prompt += ' and also generate a recomend text for online selling with at less 5 sentences, ' 
+        prompt += ' and extract no more than 10 most important tags for describing this online product in %s.'%language 
+        prompt += ' the output must be in strict JSON format: "short title": "the title you generate", "product title": "the title you generate", "recommend": "the recommend text you generate", "tags": "[important tags array you selected]". ' 
+        prompt += ' the language of all the output values must be only %s, '%language
+        prompt += ' and please notice that do not include any information about cross-border.'
         
+        print(prompt)
         openai.api_key = self.OPENAI_KEY
         response = openai.Completion.create(
           model=self.DEFAULT_OPENAI_COMPLETION_MODEL,
           prompt=prompt,
-          max_tokens=max_tokens,
-          temperature=temperature,
-          frequency_penalty=0.0,
-          presence_penalty=0.0
-        )
-        result = {
-            'title': response.choices[0]['text'].strip(),
-            'usage': response.usage
-        }
-        return result 
-    
-    
-    def __generate_full_title(self, language='English'):
-        result = self.__generate_title(
-            max_tokens=64,
-            temperature=1,
-            language=language
-        )
-        return result
-    
-    def __generate_short_title(self, language='English'):
-        result = self.__generate_title(
-            max_tokens=16,
-            temperature=0.6,
-            language=language
-        )
-        return result   
-            
-    def __generate_important_tags(self, language='English'):
-        prompt = "this is some information of a product in json format,"
-        if self.category != None:
-            prompt += " and this is a %s product;"%self.category
-        prompt += " please extract no more than 10 most important tags for describing this online product," 
-        prompt += " only in %s: %s."%(language, json.dumps(self.item_specifics))
-        
-        openai.api_key = self.OPENAI_KEY
-        response = openai.Completion.create(
-          model=self.DEFAULT_OPENAI_COMPLETION_MODEL,
-          prompt=prompt,
-          max_tokens=1024,
-          temperature=0.2,
-          frequency_penalty=0.0,
-          presence_penalty=0.0
-        )
-        result = {
-            'important_tags': response.choices[0]['text'],
-            'usage': response.usage
-        }
-        return result
-    
-    def __generate_recommend_essay(self, language='English'):
-        prompt = "this is some information of a product in json format,"
-        if self.category != None:
-            prompt += " and this is a %s product;"%self.category
-        prompt += " please help to generate a short recomend text in %s"%language
-        prompt += " for online selling: %s %s."%(self.item_name, json.dumps(self.item_specifics))
-        
-        openai.api_key = self.OPENAI_KEY
-        response = openai.Completion.create(
-          model=self.DEFAULT_OPENAI_COMPLETION_MODEL,
-          prompt=prompt,
-          max_tokens=256,
+          max_tokens=2056,
           temperature=0.6,
           frequency_penalty=0.0,
           presence_penalty=0.0
         )
+
         result = {
-            'recommend_essay': response.choices[0]['text'],
+            'context': response.choices[0]['text'],
             'usage': response.usage
         }
         return result
     
-    
-    def run(self, language='English'):
-        result = {
-            'usage': {
-                'completion_tokens': 0,
-                'prompt_tokens': 0,
-                'total_tokens': 0 
-            }
-        }
-        
-        short_title_result = self.__generate_short_title()
-        print(short_title_result)
-        result['short_title'] = short_title_result['title']
-        result['usage']['completion_tokens'] += short_title_result['usage']['completion_tokens']
-        result['usage']['prompt_tokens'] += short_title_result['usage']['prompt_tokens']
-        result['usage']['total_tokens'] += short_title_result['usage']['total_tokens']
 
-        full_title_result = self.__generate_full_title()
-        print(full_title_result)
-        result['full_title'] = full_title_result['title']
-        result['usage']['completion_tokens'] += full_title_result['usage']['completion_tokens']
-        result['usage']['prompt_tokens'] += full_title_result['usage']['prompt_tokens']
-        result['usage']['total_tokens'] += full_title_result['usage']['total_tokens']
-    
-        important_tags_result = self.__generate_important_tags()
-        print(important_tags_result)
-        result['important_tags'] = important_tags_result['important_tags']
-        result['usage']['completion_tokens'] += important_tags_result['usage']['completion_tokens']
-        result['usage']['prompt_tokens'] += important_tags_result['usage']['prompt_tokens']
-        result['usage']['total_tokens'] += important_tags_result['usage']['total_tokens']
-    
-        recommend_essay_result = self.__generate_recommend_essay()
-        print(recommend_essay_result)
-        result['recommend_essay'] = recommend_essay_result['recommend_essay']
-        result['usage']['completion_tokens'] += recommend_essay_result['usage']['completion_tokens']
-        result['usage']['prompt_tokens'] += recommend_essay_result['usage']['prompt_tokens']
-        result['usage']['total_tokens'] += recommend_essay_result['usage']['total_tokens']
-        
+    def run(self, language='English'):
+        result = self.__generate_basic_product_context(language)
+
         return result
+
     
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from saturn_engine import ProductContextGenerator
 
 
-openai_key = "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"
+openai_key = "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"
 origin_data_file = "1688_test.json"
 with open(origin_data_file, "r") as file_in:
     for line in file_in.readlines():
         obj = ProductContextGenerator(line, openai_key)
-        result = obj.run()
+        result = obj.run('English')
         print(result)
